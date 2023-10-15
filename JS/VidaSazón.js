7cceacf8968c8d763908333034ac3cd634abdcda
carlos <alalalalalagmail>
2023-10-15 02:21:18 -0600

    Cambios 10/15/23

--- JS/VidaSazón.js
+++ JS/VidaSazón.js
@@ -1,49 +1,68 @@
 //------------------------------ Dropdown menu ------------------------------
 document.addEventListener('DOMContentLoaded', function () {
   const menuLinks = document.querySelectorAll('nav ul li a'); // Selected elements
+  const menuLinksAfter = document.querySelectorAll('nav> ul > li > a::after');
 
-  menuLinks.forEach(menuLink => {
+  menuLinks.forEach((menuLink, index) => {
+
+
     const subMenu = menuLink.parentElement.querySelector('ul'); // subMenu 'ul'
+    const barsElement = document.querySelector('.animation_Bars::after');
 
-   
 
     //Visible
     menuLink.addEventListener('mouseenter', function () {
       subMenu.style.visibility = 'visible';
       subMenu.style.opacity = 1;
-     
+      menuLink.style.color = 'rgb(255, 255, 255)'
+      ;
+
+
     });
 
     //Hiden
     menuLink.addEventListener('mouseleave', function () {
       subMenu.style.visibility = 'hidden';
       subMenu.style.opacity = 0;
+      menuLink.style.color ='rgb(188, 188, 188)';
      
 
       //Visible subMenu :Hover
       subMenu.addEventListener('mouseenter', function () {
         subMenu.style.visibility = 'visible';
         subMenu.style.opacity = 1;
+        menuLink.style.color = 'rgb(255, 255, 255)'
+      
+        
 
-         //Hiden subMenu :Hover
+
+
+        //Hiden subMenu :Hover
         subMenu.addEventListener('mouseleave', function () {
           subMenu.style.visibility = 'hidden';
           subMenu.style.opacity = 0;
+          menuLink.style.color ='rgb(188, 188, 188)';
+
+         
+
+          
         });
-        
-      });
-      
-    });
-   
-  
-
-  });
-
-
  
+    
+      });
+
+
+    });
+
+
+
+  })
 });
 
-// teoria
+
+
+
+
 
 
 
@@ -55,22 +74,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 //------------------------------Dropdown menu MEDIA------------------------------ 
+
 var menu = document.querySelectorAll('.menu_toggle');
 var marginToggle = document.querySelectorAll('.margin_Toggle');
 var menuPosition = document.querySelectorAll('.ayuda_li_a_position');
 var tglWhite = document.querySelectorAll('.tgl_white');
 
 menu.forEach(function (item) {
-    item.addEventListener('click', function (i) {
-        var elemento = i.target.parentNode;
-        console.log(elemento.children);
-        elemento.children[2].classList.toggle('activo');
-        iconArrow.classList.add('.clicked');
-        
+  item.addEventListener('click', function (i) {
+    var elemento = i.target.parentNode;
+    console.log(elemento.children);
+    elemento.children[2].classList.toggle('activo');
+    iconArrow.classList.add('.clicked');
 
 
 
-    })
+
+  })
 
 
 
@@ -95,36 +115,36 @@ const minMargin = -200; // Limit of margin lower
 const maxMargin = 0;    // Limit of margin upper
 
 
-checkboxRight.addEventListener('change', function() {
-    //Limit margin to -200%
-   if (clics < -200 / -100){
-      clics ++;
+checkboxRight.addEventListener('change', function () {
+  //Limit margin to -200%
+  if (clics < -200 / -100) {
+    clics++;
 
-      let nuevoMargen = clics * -100 + '%';
+    let nuevoMargen = clics * -100 + '%';
 
     sliderButton.style.marginLeft = nuevoMargen;
 
-   }else if (clics > -200 / -100 ){
-    clics --;
-   
-    }
+  } else if (clics > -200 / -100) {
+    clics--;
+
+  }
 });
 
 checkboxLeft.addEventListener('change', function () {
-  
-   if (clics < 0 / -100) {
-      clics++;
-      }
-        // Limit margin to 0% 
-      else if (clics > 0 / -100) {
-         clics --;
-      }
-      
- 
-   nuevoMargen = clics * -100 + '%';
-   sliderButton.style.marginLeft = nuevoMargen;
+
+  if (clics < 0 / -100) {
+    clics++;
+  }
+  // Limit margin to 0% 
+  else if (clics > 0 / -100) {
+    clics--;
+  }
+
+
+  nuevoMargen = clics * -100 + '%';
+  sliderButton.style.marginLeft = nuevoMargen;
 })
 
 
- 
+
 
